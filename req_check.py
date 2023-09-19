f4f342e2cf54a4ae3ee19ec632821fd6769d92a1
Genna-C <genna@ualberta.ca>
2023-09-18 19:35:10 -0600

    change GET to personal code

--- req_check.py
+++ req_check.py
@@ -2,5 +2,5 @@ import requests
 
 print(requests.__version__)
 
-resp = requests.get("http://google.com")
+resp = requests.get("https://github.com/Genna-C/CMPUT_404_lab1/blob/main/req_check.py")
 print(resp.text)
