6a13cf8237240ae5f6c8bb0188b734fbc21592e7
Genna-C <genna@ualberta.ca>
2023-09-20 18:26:59 -0600

    modify to raw URL

--- req_check.py
+++ req_check.py
@@ -2,5 +2,5 @@ import requests
 
 print(requests.__version__)
 
-resp = requests.get("https://github.com/Genna-C/CMPUT_404_lab1/blob/main/req_check.py")
+resp = requests.get("https://raw.githubusercontent.com/Genna-C/CMPUT_404_lab1/main/req_check.py")
 print(resp.text)
